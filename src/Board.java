d0e013b5850381678b24402912f019f50e531fef
blackmarx <blackmarx@protonmail.com>
2019-11-21 21:34:18 +0500

    initial Render Test

---
+++ src/Board.java
@@ -0,0 +1,32 @@
+import javafx.scene.layout.GridPane;
+
+
+public class Board {
+
+    private static Board ourInstance = null;
+
+    private static GridPane table;
+
+    public static Board getInstance(int side) {
+        if(ourInstance == null)
+            ourInstance = new Board(side);
+        return ourInstance;
+    }
+
+    public static void destroyInstance(){ourInstance = null;}
+
+    private Board(int side) {
+        table = new GridPane();
+        for (int i = 0; i < side; i++) {
+            for (int j = 0; j < side; j++) {
+                table.add((new Block(Math.random()>0.5, Math.random()>0.5, Math.random()>0.5, Math.random()>0.5).getCell()), i, j);
+            }
+        }
+    }
+
+    public GridPane getTable(){
+        return table;
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,16 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+//        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = Board.getInstance(20).getTable();
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
