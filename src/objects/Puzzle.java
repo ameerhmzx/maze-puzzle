ebb249d1524240bd8b5f465081be5435230e2779
nmanumr <nmanumr@gmail.com>
2019-12-01 00:39:09 +0500

    removed unnecessary logging

--- src/objects/Puzzle.java
+++ src/objects/Puzzle.java
@@ -2,6 +2,7 @@ package objects;
 
 import Helpers.Constants;
 import enums.Direction;
+import layoutChanges.LayoutChanges;
 import layoutStrategies.LayoutStrategy;
 
 public class Puzzle implements Constants {
@@ -16,13 +17,12 @@ public class Puzzle implements Constants {
         this.layoutStrategy = layoutStrategy;
 
         this.board = new Board(width, height);
-        drawBoard(this.board);
-        layoutStrategy.layoutBoard(this.board).display();
-        drawBoard(this.board);
+        LayoutChanges boardChanges = layoutStrategy.layoutBoard(this.board);
+        // TODO: animate board changes
     }
 
     public static void main(String[] args) {
-        new Puzzle(6, 6, LayoutStrategy.WILSON_MAZE);
+        drawBoard(new Puzzle(6, 6, LayoutStrategy.WILSON_MAZE).board);
     }
 
     public int getSize() {
@@ -45,48 +45,48 @@ public class Puzzle implements Constants {
         return this.height;
     }
 
-    private void drawBoard(Board board) {
-        for (int x = 0; x < width * 2; x++) {
+    private static void drawBoard(Board board) {
+        for (int x = 0; x < board.getWidth() * 2; x++) {
             Cell cell = board.getCell(0, x / 2);
             if (x % 2 == 0) {
                 System.out.print(x == 0 ? "╔" : cell.hasWall(Direction.LEFT) ? "╤" : "═");
             } else {
                 System.out.print(cell.hasWall(Direction.UP) ? "═══" : "   ");
-                if (x / 2 == this.width - 1) {
+                if (x / 2 == board.getWidth() - 1) {
                     System.out.print("╗\n");
                 }
             }
         }
 
-        for (int y = 1; y < height * 2; y++) {
-            for (int x = 0; x < width * 2; x++) {
+        for (int y = 1; y < board.getHeight() * 2; y++) {
+            for (int x = 0; x < board.getWidth() * 2; x++) {
                 Cell cell = board.getCell(y / 2, x / 2);
 
                 if (y % 2 != 0 && x % 2 == 0) {
                     System.out.print(cell.hasWall(Direction.LEFT) ? x == 0 ? "║" : "│" : " ");
                 } else if (y % 2 != 0) {
                     System.out.print("   ");
-                    if (x / 2 == this.width - 1) {
+                    if (x / 2 == board.getWidth() - 1) {
                         System.out.print(cell.hasWall(Direction.RIGHT) ? "║\n" : " \n");
                     }
                 } else if (x % 2 == 0) {
                     System.out.print(x == 0 ? cell.hasWall(Direction.UP) ? "╟" : "║" : "┼");
                 } else {
                     System.out.print(cell.hasWall(Direction.UP) ? "───" : "   ");
-                    if (x / 2 == this.width - 1) {
+                    if (x / 2 == board.getWidth() - 1) {
                         System.out.print(cell.hasWall(Direction.UP) ? "╢\n" : "║\n");
                     }
                 }
             }
         }
 
-        for (int x = 0; x < width * 2; x++) {
-            Cell cell = board.getCell(height - 1, x / 2);
+        for (int x = 0; x < board.getWidth() * 2; x++) {
+            Cell cell = board.getCell(board.getHeight() - 1, x / 2);
             if (x % 2 == 0) {
                 System.out.print(x == 0 ? "╚" : cell.hasWall(Direction.LEFT) ? "╧" : "═");
             } else {
                 System.out.print(cell.hasWall(Direction.DOWN) ? "═══" : "   ");
-                if (x / 2 == this.width - 1) {
+                if (x / 2 == board.getWidth() - 1) {
                     System.out.print("╝\n");
                 }
             }
